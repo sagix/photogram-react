ec92c131efbcaf284f68c0040ba59ff2175bbf44
Nicolas Mouchel <n.mouchel@meetic-corp.com>
2017-12-23 17:48:17 +0100

    init repository

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Photogram</h1>
+          <p>This is the new Photogram</p>
+        </header>
+        <p className="App-intro">
+          Add picture and svg file
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
