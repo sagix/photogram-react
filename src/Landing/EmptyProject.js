9dc720f4a49d22c2d0499cdb3c7a6e4e7ffc1423
Nicolas Mouchel <n.mouchel@meetic-corp.com>
2019-08-24 20:31:13 +0200

    Add tutorial

--- src/Landing/EmptyProject.js
+++ src/Landing/EmptyProject.js
@@ -18,28 +18,30 @@ class EmptyProject  extends Component{
   render(){
       return (
         <div className="empty-project-layout">
-          <EmptyCell className="empty-project-item"
-            src="illus/undraw_learning_2q1h.svg"
-            title="Learn"
-            desc="Read our tutorial to learn how to create a new project"
-            />
-            <form>
-                <input type="file" id="new-project-file"
-                    webkitdirectory={true.toString()}
-                    directory={true.toString()}
-                    multiple={true}
-                    onChange={(event) => {
-                        this.props.onNewProject(event.target.files)
-                        event.target.form.reset()
-                    }}/>
-                <label htmlFor="new-project-file">
-                  <EmptyCell className="empty-project-item"
-                    src="/illus/undraw_upload_image_iwej.svg"
-                    title="Create"
-                    desc="Import your pictures and csv file"
-                    />
-                </label>
-            </form>
+          <a href="/tutorial">
+            <EmptyCell className="empty-project-item"
+              src="illus/undraw_learning_2q1h.svg"
+              title="Learn"
+              desc="Read our tutorial to learn how to create a new project"
+              />
+          </a>
+          <form>
+              <input type="file" id="new-project-file"
+                  webkitdirectory={true.toString()}
+                  directory={true.toString()}
+                  multiple={true}
+                  onChange={(event) => {
+                      this.props.onNewProject(event.target.files)
+                      event.target.form.reset()
+                  }}/>
+              <label htmlFor="new-project-file">
+                <EmptyCell className="empty-project-item"
+                  src="/illus/undraw_upload_image_iwej.svg"
+                  title="Create"
+                  desc="Import your pictures and csv file"
+                  />
+              </label>
+          </form>
 
           <EmptyCell className="empty-project-item"
             src="/illus/undraw_Presentation_62e1.svg"
