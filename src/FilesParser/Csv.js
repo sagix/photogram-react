6ba8ddb6781b354b882726ad3cd1e9f4f9eeee44
Nicolas Mouchel <nmo@octo.com>
2023-01-08 15:17:28 +0100

    use async/await instead of promise for csv

--- src/FilesParser/Csv.js
+++ src/FilesParser/Csv.js
@@ -2,31 +2,37 @@ import Papa from 'papaparse';
 
 export default class Csv {
 
-    execute(files) {
-        let result = Array.from(files).filter(file => file.name === "data.csv")
-        if (result.length === 0) {
-            return Promise.reject(new Error('Could not found data.csv'))
+    async execute(files) {
+        const csv = Array.from(files).find(file => file.name === "data.csv");
+        if (csv === undefined) {
+            throw new Error('Could not found data.csv');
         }
-        return new Promise((resolve, reject) => Papa.parse(result[0], {
+        const parsed = await this.parseCsv(csv);
+
+        return parsed.data.map(r => {
+            const id = this.parseId(r[0])
+            if (id !== null) {
+                return {
+                    id: id,
+                    sequence: id,
+                    action: r[1],
+                    label: (r[2] || "").trim(),
+                    periode: (r[3] || "").trim(),
+                    fx: r[4],
+                };
+            } else {
+                return null;
+            }
+        }).filter(line => line !== null);
+
+
+    }
+
+    async parseCsv(file) {
+        return new Promise((resolve, reject) => Papa.parse(file, {
             complete: resolve,
             error: reject
-        })).then(parsed => {
-            var result = [];
-            for (let r of parsed.data) {
-                const id = this.parseId(r[0])
-                if (id !== null) {
-                    result.push({
-                        id: id,
-                        sequence: id,
-                        action: r[1],
-                        label: (r[2] || "").trim(),
-                        periode: (r[3] || "").trim(),
-                        fx: r[4],
-                    })
-                }
-            }
-            return result
-        })
+        }));
     }
 
     parseId(id) {
