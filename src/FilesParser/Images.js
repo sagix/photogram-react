7eec0c5ed936873f16d770fec4a92e42d0bd43e2
Nicolas Mouchel <nmo@octo.com>
2023-01-08 15:07:35 +0100

    Use async/await instead of promise for Images

--- src/FilesParser/Images.js
+++ src/FilesParser/Images.js
@@ -13,51 +13,44 @@ class Images {
         return new Images(new StubbedCaches(factory))
     }
 
-    execute(identifier, files) {
-        let imageType = /^image\//;
-        return Promise.all(
-            Array.from(files)
-                .filter(file => imageType.test(file.type))
-                .map(file => readAsArrayBuffer(file))
-        ).then(fileResults =>
-            Promise.all(
-                fileResults.map(fileResult => this._writeFile(identifier, fileResult))
-            )
-        )
+    async execute(identifier, files) {
+        const imageType = /^image\//;
+        const images = Array.from(files).filter(file => imageType.test(file.type));
+        return Promise.all(images.map(async file => {
+            const fileResult = await readAsArrayBuffer(file);
+            return this._writeFile(identifier, fileResult);
+        }));
+    
     }
 
-    clear(identifier) {
-        return this._caches.delete('image-cache-' + identifier).then(result => {
-            if (result) {
-                return Promise.resolve(null);
-            } else {
-                return Promise.reject(Error(`Cannot delete the cache: ${identifier}`));
-            }
-        })
+    async clear(identifier) {
+        const result = await this._caches.delete('image-cache-' + identifier)
+        if (result) {
+            return null;
+        } else {
+            throw Error(`Cannot delete the cache: ${identifier}`);
+        }
     }
 
-    _writeFile(identifier, event) {
-        return this._caches.open('image-cache-' + identifier)
-            .then(cache => {
-                let h = new Headers()
-                h.append("Content-Type", event.file.type)
-                h.append("Content-Length", event.file.size)
+    async _writeFile(identifier, event) {
+        const cache = await this._caches.open('image-cache-' + identifier);
+        const headers = new Headers()
+        headers.append("Content-Type", event.file.type)
+        headers.append("Content-Length", event.file.size)
 
-                return cache.put(
-                    new Request(`/project/${identifier}/images/${event.file.name}/`),
-                    new Response(event.result, {
-                        status: 200,
-                        statusText: "From Cache",
-                        headers: h
-                    })
-                );
-            }).then(_ => {
-                return {
-                    name: event.file.name,
-                    url: `/project/${identifier}/images/${event.file.name}/`
-                }
+        await cache.put(
+            new Request(`/project/${identifier}/images/${event.file.name}/`),
+            new Response(event.result, {
+                status: 200,
+                statusText: "From Cache",
+                headers: headers
             })
+        );
 
+        return {
+            name: event.file.name,
+            url: `/project/${identifier}/images/${event.file.name}/`
+        }
     }
 }
 
