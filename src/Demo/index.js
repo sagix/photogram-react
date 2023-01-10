5cd9ecf3a5043bcad8d7349432449e475a832400
Nicolas Mouchel <nmo@octo.com>
2023-01-10 16:52:11 +0100

    Remove custom csv and data

--- src/Demo/index.js
+++ src/Demo/index.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import {Helmet} from "react-helmet";
-import Application from './data';
+import { Helmet } from "react-helmet";
+import Application from '../Application';
+import ProjectsDataSource from '../Application/ProjectsDataSource';
 import Page from '../Project/Page'
+import Csv from '../FilesParser/Csv';
 
 class Project extends Component {
-  constructor(props){
-      super(props);
-      this.application = Application.create();
-    }
+  constructor(props) {
+    super(props);
+    this.application = Application.create();
+    this.state = {};
+  }
 
-  render(){
-      return (
-        <div>
-          <Helmet>
-             <title>Photogram - Try the tool for assistant editors</title>
-             <meta name="description" content="Learn and play with our demo data set" />
-          </Helmet>
-          <Page application={this.application} />
-        </div>
-      );
+  async componentDidMount() {
+    const csv = await this.createDataCsv();
+    const data = await new Csv().execute([csv]);
+    const base = new URL(window.location.href);
+    data.map(d => {
+      return Object.assign(d, {
+        url: new URL(`/demo/${d.id}.jpg`, base)
+      })
+    })
+    const project = {
+      key: "demo",
+      name: "Le fardeau (démo)",
+      data: data,
+      colors: {},
+      template: "small",
     }
+    this.setState({
+      application: Application.createNull({
+        dataSource: ProjectsDataSource.createNull({ projects: [project] })
+      })
+    })
+  }
+
+  async createDataCsv() {
+    let response = await fetch(new URL("demo/data.csv", new URL(window.location.href)));
+    let data = await response.blob();
+    let metadata = {
+      type: 'text/csv'
+    };
+    return new File([data], "data.csv", metadata);
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Photogram - Try the tool for assistant editors</title>
+          <meta name="description" content="Learn and play with our demo data set" />
+        </Helmet>
+        {this.state.application
+          ? <Page application={this.state.application} id="demo" />
+          : <div />}
+      </div>
+    );
+  }
 }
 
 export default Project;
