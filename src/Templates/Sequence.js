b96f6c4af44a885badad615f1ae18c071ef20fe1
Nicolas Mouchel <nicolas.mouchel@proton.ch>
2023-07-05 19:19:06 +0200

    fix label with white color too soon

--- src/Templates/Sequence.js
+++ src/Templates/Sequence.js
@@ -7,7 +7,7 @@ class Sequence extends Component {
         var g = parseInt(hexcolor.substring(3,5),16);
         var b = parseInt(hexcolor.substring(5,7),16);
         var yiq = ((r*299)+(g*587)+(b*114))/1000;
-        return (yiq >= 128) ? 'dark' : 'light';
+        return (yiq >= 192) ? 'dark' : 'light';
     }
 
     render() {
